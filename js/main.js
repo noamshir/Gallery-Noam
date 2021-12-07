7441159314c5a2da6036b6752f2500b7d7fa5f34
Noam Shir <noamshirm@gmail.com>
2021-12-07 18:52:49 +0200

    picture bug fixed

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function renderPortfolio() {
          <i class="fa fa-plus fa-3x"></i>
        </div>
      </div>
-     <img class="img-fluid" src="img/portfolio/game.png" alt="">
+     <img class="img-fluid" src="img/portfolio/Game.png" alt="">
    </a>
    <div class="portfolio-caption">
      <h4>${proj.name}</h4>
@@ -37,33 +37,30 @@ function renderProtfolioModal(el) {
   $(".desc").html(proj.desc);
   var date = formatDate(proj.publishedAt);
   $(".date").text(date);
-  var labels =  proj.labels
+  var labels = proj.labels;
   var str = labels.toString();
-  $('.pj-labels').text(str);
-  $('.pj-link').attr('href',proj.url);
+  $(".pj-labels").text(str);
+  $(".pj-link").attr("href", proj.url);
 }
 
 function formatDate(date) {
   var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
 
-  if (month.length < 2) 
-      month = '0' + month;
-  if (day.length < 2) 
-      day = '0' + day;
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
 
-  return [day, month, year].join('-');
+  return [day, month, year].join("-");
 }
 
-function sendToEmail()
-{
-  var email = $('#email').val();
-  var subject =$('#subject').val();
-  var body  = $('.msg').val();
-  console.log(email, subject , body)
-  window.open(url=`https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`)
+function sendToEmail() {
+  var email = $("#email").val();
+  var subject = $("#subject").val();
+  var body = $(".msg").val();
+  console.log(email, subject, body);
+  window.open(
+    (url = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`)
+  );
 }
-
-
